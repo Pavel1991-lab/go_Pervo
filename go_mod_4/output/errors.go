dcbd86b2e6b32e25cf1b315a61be294d8fa45d68
Pavel <pavel@example.com>
2025-08-19 22:16:12 +0300

    12.8

--- go_mod_4/output/errors.go
+++ go_mod_4/output/errors.go
@@ -3,15 +3,28 @@ package output
 import "github.com/fatih/color"
 
 func PrintError(value any) {
-
-	switch t := value.(type) {
-	case string:
-		color.Red(t)
-	case int:
-		color.Red("Password error: %d", t)
-	case error:
-		color.Red(t.Error())
-	default:
-		color.Red("Undefind error")
+	intval, ok := value.(int)
+	if ok {
+		color.Red("Pas error: %d", intval)
+		return
 	}
+	strValue, ok := value.(string)
+	if ok {
+		color.Red(strValue)
+	}
+	errorValue, ok := value.(error)
+	if ok {
+		color.Red(errorValue.Error())
+	}
+	color.Red("Undefind error")
+	// switch t := value.(type) {
+	// case string:
+	// 	color.Red(t)
+	// case int:
+	// 	color.Red("Password error: %d", t)
+	// case error:
+	// 	color.Red(t.Error())
+	// default:
+	// 	color.Red("Undefind error")
+	// }
 }
